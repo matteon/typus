ff8a30fc82272aea0c9628ead30182aa216b7da2
matteo <matteo@wonsys.net>
2010-08-28 18:47:44 +0200

    Try again to fix issue in configr.rb template

--- lib/generators/templates/config/initializers/configr.rb
+++ lib/generators/templates/config/initializers/configr.rb
@@ -1,3 +1,3 @@
-default_configuration     = YAML.load(Rails.root.join('config', 'configr.yml'))
-environment_configuration = YAML.load(Rails.root.join('config', 'environments', "\#{Rails.env}.yml"))
-AppConfig = Configr::Configuration.configure(YAML.dump(default_configuration.merge(environment_configuration)))
+# default_configuration     = YAML.load(Rails.root.join('config', 'configr.yml'))
+# environment_configuration = YAML.load(Rails.root.join('config', 'environments', "#{Rails.env}.yml"))
+# AppConfig = Configr::Configuration.configure(YAML.dump(default_configuration.merge(environment_configuration)))
