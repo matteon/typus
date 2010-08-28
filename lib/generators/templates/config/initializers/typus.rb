a6fed703f8118780ca867a2cda808cbfb1f52e00
matteo <matteo@wonsys.net>
2010-08-28 15:17:27 +0200

    Made a few changes in default typus setup file

--- lib/generators/templates/config/initializers/typus.rb
+++ lib/generators/templates/config/initializers/typus.rb
@@ -7,20 +7,20 @@ Typus.setup do |config|
   # Configure the e-mail address which will be shown in Admin::Mailer.
   # When this option is define Typus will allow admin users to recover
   # lost passwords.
-  # config.mailer_sender = "admin@example.com"
+  config.mailer_sender = Configurator.admin.email
 
   # Define file attachment settings.
-  # config.file_preview = :typus_preview
-  # config.file_thumbnail = :typus_thumbnail
+  config.file_preview = :typus_preview
+  config.file_thumbnail = :typus_thumbnail
 
   # Authentication: :none, :http_basic
-  # config.authentication = :none
+  config.authentication = :session
 
   # Define username and password for http authentication
   # config.username = "admin"
-  # config.password = "columbia"
+  # config.password = "password"
 
   # Define available languages on the admin interface.
-  # config.available_locales = [:en]
+  config.available_locales = [:it, :en]
 
 end
