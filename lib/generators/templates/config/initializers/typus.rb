6b95b30876cba313374b1f5332cf044622ee70cb
matteo <matteo@wonsys.net>
2010-08-28 15:54:23 +0200

    Added configr gem for configuration

--- lib/generators/templates/config/initializers/typus.rb
+++ lib/generators/templates/config/initializers/typus.rb
@@ -7,7 +7,7 @@ Typus.setup do |config|
   # Configure the e-mail address which will be shown in Admin::Mailer.
   # When this option is define Typus will allow admin users to recover
   # lost passwords.
-  config.mailer_sender = Configurator.admin.email
+  config.mailer_sender = AppConfig.admin.email
 
   # Define file attachment settings.
   config.file_preview = :typus_preview
