2c5a766255a467c7bb2face14fb2e6e6564ad359
matteo <matteo@wonsys.net>
2010-08-28 14:51:42 +0200

    Fixed bug in config relationship

--- lib/generators/templates/config/initializers/typus_migration.rb
+++ lib/generators/templates/config/initializers/typus_migration.rb
@@ -4,15 +4,14 @@ Typus.setup do |config|
   config.authentication = :session
 
   # Define the default root.
-  # config.master_role = "admin"
+  config.master_role = 'Administrator'
 
   # Define relationship table.
-  # config.relationship = "typus_users"
+  config.relationship = "<%= options[:user_class_name].tableize %>"
 
   # Define user_class_name and user_fk.
   config.user_class_name = "<%= options[:user_class_name] %>"
 
   # Define the user_fk
   config.user_fk = "<%= options[:user_fk] %>"
-
 end
